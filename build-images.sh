5f0d816978e1b99ed6f855286fc4fd6eab45473d
mrmarkuz <mrmarkuz666@gmail.com>
2026-07-20 21:11:21 +0200

    Add image label volume for immich-data

--- build-images.sh
+++ build-images.sh
@@ -42,6 +42,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
+	--label="org.nethserver.volumes=immich-data" \
     --label="org.nethserver.images=ghcr.io/immich-app/postgres:14-vectorchord0.4.3-pgvectors0.2.0@sha256:bcf63357191b76a916ae5eb93464d65c07511da41e3bf7a8416db519b40b1c23 ghcr.io/immich-app/immich-server:v3.0.3 ghcr.io/immich-app/immich-machine-learning:v3.0.3 docker.io/valkey/valkey:9@sha256:4963247afc4cd33c7d3b2d2816b9f7f8eeebab148d29056c2ca4d7cbc966f2d9" \
     "${container}"
 # Commit the image
